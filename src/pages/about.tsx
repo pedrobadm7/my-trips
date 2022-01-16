a83eae4bf575171eab35fb523b1863b9846e7cb7
Pedro Barros <pedrobars7a@gmail.com>
2022-01-16 15:23:52 -0300

    criando metodo getStaticPaths

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,36 @@
 import client from 'graphql/client'
 import { GET_PAGES } from 'graphql/queries'
-import AboutTemplate from 'templates/About'
+import { useRouter } from 'next/router'
+import PageTemplate from 'templates/Pages'
 
 export default function AboutPage() {
-  return <AboutTemplate />
+  const router = useRouter()
+
+  /* Retorna qualquer coisa enquanto a página é criada */
+  if (router.isFallback) return null
+
+  return <PageTemplate />
 }
+
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 })
+
+  const paths = pages.map(({ slug }) => {
+    return {
+      params: { slug }
+    }
+  })
+
+  return { paths, fallback: true }
+}
+
+// export const getStaticProps = async () => {
+//   return {
+//     props: {
+//       pages
+//     }
+//   }
+// }
 
 /**
  * getStaticPaths => serve para gerar as urls em build time /about, /trip/petropolis
@@ -12,13 +38,3 @@ export default function AboutPage() {
  * getServerSideProps => serve para buscar dados da página (props) - runtime - toda requisição (bundle fica no server)
  * getInitialProps => serve para buscar dados da página (props) - runtime - toda requisição (bundle também vem para o client) - hydrate
  */
-
-export const getStaticProps = async () => {
-  const { pages } = await client.request(GET_PAGES)
-
-  return {
-    props: {
-      pages
-    }
-  }
-}
