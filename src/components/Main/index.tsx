d367b2a386fb43d1544029f72a834ca02e8de9ff
Pedro Barros <pedrobars7a@gmail.com>
2022-01-16 09:35:46 -0300

    Criando Mapa com Leaflet

--- src/components/Main/index.tsx
+++
@@ -1,8 +0,0 @@
-const Main = () => (
-  <div>
-    <h1>MyTrips</h1>
-    <a href="/">link</a>
-  </div>
-)
-
-export default Main
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,12 @@ const App = ({ Component, pageProps }: AppProps) => {
           name="description"
           content="A simple project to show my favorites spots in the world"
         />
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+          integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+          crossOrigin=""
+        />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
