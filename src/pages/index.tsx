f0bc8f93020f032fb1530028af60194f7e42a119
Pedro Barros <pedrobars7a@gmail.com>
2022-01-16 11:49:22 -0300

    removendo mock do Map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,5 @@ import dynamic from 'next/dynamic'
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return (
-    <Map
-      places={[
-        {
-          id: '2',
-          name: 'Contagem',
-          slug: 'contagem',
-          location: {
-            latitude: -19.9386,
-            longitude: -44.0529
-          }
-        }
-      ]}
-    />
-  )
+  return <Map />
 }
