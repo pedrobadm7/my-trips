3e709b0aa4b1a44d33dafe4f493e8017c2d049dd
Pedro Barros <pedrobars7a@gmail.com>
2022-01-22 17:47:54 -0300

    add Google Analytics to project

---
+++ src/components/Analytics/index.tsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/no-danger */
+import React from 'react'
+
+const Analytics = () => (
+  <>
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS_ID}`}
+    />
+
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING}', {
+            page_path: window.location.pathname,
+          })
+        `
+      }}
+    />
+  </>
+)
+
+export default Analytics
